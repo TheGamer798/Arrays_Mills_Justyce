e3f02f10afcf5558be7fb66bf52830c201b0f498
TheGamer798 <111769499+TheGamer798@users.noreply.github.com>
2023-04-11 09:28:05 -0500

    Initial commit

---
+++ Arrays_Mills_Justyce/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arrays_Mills_Justyce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a whole number for array size");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[arraySize];
+            int sum = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("input another whole number to add to the array");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                sum += numbers[i];
+            }
+
+            Console.WriteLine($"the sum of the array is {sum}\n");
+            Array.Sort(numbers);
+            Console.WriteLine("Array:");
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
